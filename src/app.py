8e23d02c8863f3a2b1e8463ae237df439c016e8d
Moises P.N <137330340+DevPE2024@users.noreply.github.com>
2023-07-30 15:54:23 -0300

    👌 IMPROVE: tudo novo e funcionando

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
 import time
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import Edge
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from bs4 import BeautifulSoup
+from selenium.webdriver import Edge
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.common.exceptions import NoSuchElementException
+
 
 # Configura as opções do Edge
 options = Options()
@@ -18,7 +20,9 @@ service = Service(EdgeChromiumDriverManager().install())
 # Cria uma nova instância do Microsoft Edge
 driver = Edge(service=service, options=options)
 
-driver.implicitly_wait(10)  # espera até 10 segundos antes de lançar uma NoSuchElementException
+driver.implicitly_wait(
+    10
+)  # espera até 10 segundos antes de lançar uma NoSuchElementException
 
 url = "https://www.arbety.com/games/double"
 driver.get(url)
@@ -26,28 +30,41 @@ driver.get(url)
 # Armazena o código HTML atual da página
 html_antigo = driver.page_source
 
+# Armazena as linhas de dados já impressas
+dados_impressos = set()
+
 try:
     while True:
         try:
             # Verifica se os elementos estão presentes na página
             elemento_pai = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'items')]"))
+                EC.presence_of_element_located(
+                    (By.XPATH, "//div[contains(@class, 'items')]")
+                )
             )
 
             # Executa o código para pegar os elementos filhos
             elementos_filhos = elemento_pai.find_elements(By.XPATH, "./*")
             for elemento in elementos_filhos:
-                soup = BeautifulSoup(elemento.get_attribute('innerHTML'), 'html.parser')
-                div = soup.find('div')
+                soup = BeautifulSoup(elemento.get_attribute("innerHTML"), "html.parser")
+                div = soup.find("div")
                 if div:
-                    cor = div.get('class')[1] if len(div.get('class')) > 1 else None
-                    aria_label = div.get('aria-label')
+                    cor = div.get("class")[1] if len(div.get("class")) > 1 else None
+                    aria_label = div.get("aria-label")
                     numero = div.text
                     if aria_label:
                         data, hora = aria_label.split(", ")
                     else:
                         data, hora = None, None
-                    print(f"Cor: {cor}, Data: {data}, Hora: {hora}, Número: {numero}")
+
+                    # Monta a linha de dados
+                    linha_dados = f"Cor: {cor}, Data: {data}, Hora: {hora}, Número: {numero}"
+
+                    # Verifica se a linha de dados já foi impressa
+                    if linha_dados not in dados_impressos:
+                        # Se não, imprime a linha de dados, e a adiciona ao conjunto de dados impressos
+                        print(linha_dados)
+                        dados_impressos.add(linha_dados)
 
             # Armazena o código HTML atual da página
             html_atual = driver.page_source
@@ -58,9 +75,9 @@ try:
                 driver.refresh()
 
             # Espera por 30 segundos antes de verificar novamente
-            time.sleep(20)
+            time.sleep(15)
 
-        except Exception as e:
+        except NoSuchElementException as e:
             print(e)
             driver.quit()
             break
